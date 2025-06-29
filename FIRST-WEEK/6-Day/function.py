4a176f1d38e088054d8ec9e9ec20b5b862de5189
biniam <biniamabayneh9@gmail.com>
2025-06-29 04:24:18 -0700

    function in python are committed

---
+++ FIRST-WEEK/6-Day/function.py
@@ -0,0 +1,9 @@
+#function in python
+print("start!")
+def greet_user() :
+    print("hey there ")
+    print("Welcome to aboard")
+
+
+greet_user()
+print("End!")
---
+++ FIRST-WEEK/6-Day/prameters.py
@@ -0,0 +1,7 @@
+def greet_users(first_name,last_name):
+    print(f"Hi {first_name} {last_name}!")
+    print("welcome to aboard \n")
+print("Start!")
+greet_users("biniam","abayneh ")
+greet_users(last_name="john",first_name="smith")
+print("End ")
