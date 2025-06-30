d8d1dce356892f5fc7897dc87c72ea231333e6ea
biniam <biniamabayneh9@gmail.com>
2025-06-29 23:08:32 -0700

    function in python are committed

--- FIRST-WEEK/6-Day/class.py
+++ FIRST-WEEK/6-Day/class.py
@@ -1,8 +1,12 @@
-class Point :
-    def move(self):
-        print("move")
+class Point:
     def draw(self):
         print("draw")
+    def win(self):
+        print("win the game")
+        x,y = 40,20,
+        z= x %y
+        print(z)
+
 
 point1 = Point()
-point1.draw()
+Point().win()
