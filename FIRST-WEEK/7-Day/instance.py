959df52a6d2ce23c0b0eb5f15b92df411dcdb2fd
biniam <biniamabayneh9@gmail.com>
2025-07-01 06:19:09 -0700

    "list of array files are added"

---
+++ FIRST-WEEK/7-Day/instance.py
@@ -0,0 +1,20 @@
+# a = 'Biniam Abayneh'
+# print(isinstance(a,int))
+#
+#
+# a = [1,2,3,4,5,6,7,8,9,10]
+# b= 0
+# for c in range(10):
+#     for f in range(9):
+#         print(f)
+#     print(c)
+
+
+def output(arrivalTime, delayedTime):
+    output = arrivalTime + delayedTime
+
+    print(' Arrival time of the train was 15:00 hours. It is delayed by 5 hours. Now it will reach at 15+5 = 20 (20:00 hours).', output)
+
+
+output(15, 5)
+
